d60aecaee054c21b23e9166e9147dd18ce08c707
Akaash Kolluri <akaashkolluri@akaashs-air.lan>
2021-11-22 22:09:07 -0600

    attempted to make video play, but it didn't work

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,22 @@ export default function Home() {
       {/* <Header /> */}
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>Prospective AI</h1>
-
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
+          <div className={styles.titleContainer}>
+            <p style={{ flex: 1 }} />
+            <h1 className={styles.title}>Prospective AI</h1>
+            <p style={{ height: 10 }} />
+            <h2 className={styles.tagline}> Educate, Innovate, Inspire</h2>
+            <p style={{ flex: 2 }} />
+          </div>
+          <h2>
+            {" "}
+            Prospective AI is an organization dedicated to making AI accessible
+            and available to students of all ages. We rely on donors to provide
+            us with the resources necessary to keep our programs and workshops
+            running year-round Your donation is crucial in allowing us to
+            provide valuable resources and quality education to our students.
+          </h2>
+          <h2> Our Offers </h2>
           <div className={styles.grid}>
             <a href="https://nextjs.org/docs" className={styles.card}>
               <h2>Events &rarr;</h2>
