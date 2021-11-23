122bf0fc4609f8032f5a5cd2b0c3d60844239b7b
Akaash Kolluri <akaashkolluri@akaashs-air.lan>
2021-11-22 20:50:09 -0600

    updated style and trying to add logo

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
-import { Menu, Affix, Card } from "antd";
+import { Menu, Affix, Card, Image } from "antd";
 import styles from "../styles/Home.module.css";
+import logo from "../public/logo.png";
 
 class Header extends Component {
   render() {
@@ -8,7 +9,9 @@ class Header extends Component {
       <Affix>
         <Menu className={styles.header}>
           <p style={{ flex: 1 }} />
-          <a href="/">Prospective AI</a>
+          <a href="/">
+            <img src={logo} alt="Logo" />
+          </a>
           <p style={{ flex: 3 }} />
           <Menu.Item key="About">About</Menu.Item>
           <Menu.Item key="Offerings">Offerings</Menu.Item>
