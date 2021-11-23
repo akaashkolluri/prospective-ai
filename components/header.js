75048545e4b00d72865b25f74003a944f7e95739
Akaash Kolluri <arkolluri@gmail.com>
2021-11-23 10:21:17 -0600

    fixed / error

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { Menu, Affix, Card, Image } from "antd";
 import styles from "../styles/Home.module.css";
+import { Link } from "next/link";
 
 class Header extends Component {
   render() {
@@ -8,13 +9,13 @@ class Header extends Component {
       <Affix>
         <Menu className={styles.header}>
           <p style={{ flex: 1 }} />
-          <a href="/">
+          <Link href="/">
             <img
               src="/logo.png"
               alt="Logo"
               style={{ height: 70, margin: 10 }}
             />
-          </a>
+          </Link>
           <p style={{ flex: 20 }} />
           <Menu.Item key="About">About</Menu.Item>
           <Menu.Item key="Offerings">Offerings</Menu.Item>
