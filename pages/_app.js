33e2592ee159a477fa160f2ef13dee5d8f2f40e1
Akaash Kolluri <arkolluri@gmail.com>
2021-11-25 11:54:49 -0600

    removed maintenance attempt

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,7 @@ import "../styles/globals.css";
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-  return [
-    process.env.MAINTENANCE === "1" ? null : <Component {...pageProps} />,
-  ];
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
