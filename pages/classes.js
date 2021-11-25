98648a20362cf948c52a74147bca95a8754cb9d5
Akaash Kolluri <arkolluri@gmail.com>
2021-11-24 23:23:46 -0600

    infrastructure for events and classes page

---
+++ pages/classes.js
@@ -0,0 +1,14 @@
+import {} from "antd";
+import styles from "../styles/Classes.module.css";
+
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function Classes() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
