0092050225ed1dcf78ae4cc5553e55ee5974f116
Akaash Kolluri <arkolluri@gmail.com>
2021-11-25 11:00:30 -0600

    added maintenance mode for app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import "../styles/globals.css";
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return [
+    process.env.MAINTENANCE === "1" ? null : <Component {...pageProps} />,
+  ];
 }
 
 export default MyApp;
