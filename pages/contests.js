f4f84010d56e70a457e3f038203159f3fa1a26b1
Akaash Kolluri <arkolluri@gmail.com>
2021-11-26 15:24:58 -0600

    added new backgrounds + construction page + draft for events and contests pages

--- pages/contests.js
+++ pages/contests.js
@@ -1,15 +1,23 @@
 import {} from "antd";
-import styles from "../styles/Events.module.css";
+import styles from "../styles/Contests.module.css";
 
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Construction from "../components/construction";
+import Link from "next/link";
 
 export default function Contests() {
   return (
     <div>
       <Header />
-      <Construction />
+      <div className={styles.container}>
+        <div style={{ flex: 1 }} />
+        <h2>Contests at Prospective AI are coming soon</h2>
+        <Link href="/">
+          <a> Return </a>
+        </Link>
+        <div style={{ flex: 2 }} />
+      </div>
       <Footer />
     </div>
   );
--- pages/events.js
+++ pages/events.js
@@ -4,12 +4,28 @@ import styles from "../styles/Events.module.css";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Construction from "../components/construction";
+import Link from "next/link";
 
 export default function Events() {
   return (
     <div>
       <Header />
-      <Construction />
+      <div className={styles.container}>
+        <h2>Upcoming Events</h2>
+        <div className={styles.grid}>
+          <Link href="/events">
+            <a className={styles.card}>
+              <h2>Social Impact of AI</h2>
+              <p>
+                Learn how AI is affecting our world, and get
+                <b> inspired </b> to make a difference
+              </p>
+              <h3> Sign Up</h3>
+            </a>
+          </Link>
+        </div>
+        <div style={{ flex: 1 }} />
+      </div>
       <Footer />
     </div>
   );
