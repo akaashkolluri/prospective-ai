367c3481db77cfa474c80c6aa87bad8ada5961e4
Akaash Kolluri <arkolluri@gmail.com>
2021-11-25 19:08:37 -0600

    added contests infrastructure

---
+++ pages/contests.js
@@ -0,0 +1,14 @@
+import {} from "antd";
+import styles from "../styles/Events.module.css";
+
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function Contests() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
