66bd95bc919537ef0656ff7a80460b302b15a056
Akaash Kolluri <arkolluri@gmail.com>
2021-11-27 12:28:36 -0600

    added new redirects to instagram and discord

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/prospective.ai/",
+        permanent: true,
+      },
+      {
+        source: "/discord",
+        destination: "https://discord.gg/vYSCGAsm3c",
+        permanent: true,
+      },
+    ];
+  },
+};
